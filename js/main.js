8841da31a5bf8520c226242c49a11db9619dbfa1
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-10 13:06:44 -0600

    adding home page color scheme and buttons

--- js/main.js
+++ js/main.js
@@ -3,3 +3,14 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
 });
+
+
+
+
+//make an object with all the images and corresponding letter/numbe value
+
+//dom onchange display image in main focus box, add image to footer
+
+//dom on click reset button, clear footer
+
+//full alphabet view?
