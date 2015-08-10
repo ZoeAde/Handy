ec6930a0db5d127b9a27cb9a163e11713ef25d96
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-10 13:30:18 -0600

    creating jQuery variable for input box

--- js/main.js
+++ js/main.js
@@ -2,6 +2,13 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
+  var inputBtn = $('.input__input');
+
+  inputBtn.on('keyup', function() {
+    console.log(inputBtn.html());
+    // $inputBtn.empty();
+  });
+
 });
 
 
@@ -9,7 +16,7 @@ $(document).on('ready', function() {
 
 //make an object with all the images and corresponding letter/numbe value
 
-//dom onchange display image in main focus box, add image to footer
+//dom onkeydown display image in main focus box, add image to footer
 
 //dom on click reset button, clear footer
 
