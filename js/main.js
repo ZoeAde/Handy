57289190d764e1367d90c600872af3fb743b9436
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-10 18:03:07 -0600

    adding footer with appended letters from input box

--- js/main.js
+++ js/main.js
@@ -2,11 +2,32 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
-  var inputBtn = $('.input__input');
+  var footer = $('#footer')
+  var letterBox = $('.input__input');
 
-  inputBtn.on('keyup', function() {
-    console.log(inputBtn.html());
-    // $inputBtn.empty();
+  //when text is entered in input box, the corresponding image appears
+  letterBox.on('keyup', function() {
+    var letter = letterBox.val();
+    footer.append(letter);
+    letterBox.val("");
+    letterBox.attr("placeholder", "");
+
+  });
+
+
+
+
+//reset button clears images/html from footer
+  var resetBtn = $('#resetBtn');
+  resetBtn.on('click', function() {
+    footer.text('');
+    letterBox.attr("placeholder", "Type Letter Here");
+  });
+
+//play button plays through array index in footer
+  var playBtn = $('#playBtn');
+  playBtn.on('click', function() {
+    //for loop with time delay?
   });
 
 });
@@ -14,7 +35,7 @@ $(document).on('ready', function() {
 
 
 
-//make an object with all the images and corresponding letter/numbe value
+//make an object with all the images and corresponding letter/number value
 
 //dom onkeydown display image in main focus box, add image to footer
 
