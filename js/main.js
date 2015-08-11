430eb1d47ef125ddbdcbf6430d564fc5dbd9e08e
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-10 21:56:08 -0600

    adding header and appending text

--- js/main.js
+++ js/main.js
@@ -2,33 +2,40 @@
 
 $(document).on('ready', function() {
   console.log('sanity check!');
-  var footer = $('#footer')
+  var header = $('#header');
   var letterBox = $('.input__input');
 
   //when text is entered in input box, the corresponding image appears
   letterBox.on('keyup', function() {
     var letter = letterBox.val();
-    footer.append(letter);
+    header.append(letter);
     letterBox.val("");
     letterBox.attr("placeholder", "");
-
   });
 
+//not working
+  // letterBox.off('click', function() {
+  //   letterBox.attr("placeholder", "Type Letter Here");
+  // });
 
-
+//not working
+//button hover color change
+// $('.btn').hover(function () {
+//   $(this).css("color", "#985CFF");
+// });
 
 //reset button clears images/html from footer
   var resetBtn = $('#resetBtn');
   resetBtn.on('click', function() {
-    footer.text('');
+    header.text('');
     letterBox.attr("placeholder", "Type Letter Here");
   });
 
 //play button plays through array index in footer
-  var playBtn = $('#playBtn');
-  playBtn.on('click', function() {
-    //for loop with time delay?
-  });
+  // var playBtn = $('#playBtn');
+  // playBtn.on('click', function() {
+  //   //for loop with time delay?
+  // });
 
 });
 
