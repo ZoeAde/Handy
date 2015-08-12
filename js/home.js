8d63a8f0f2931c7364c7696800571bb70a159862
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-12 09:52:38 -0600

    Adding landing page and fade in/fade out functionality

---
+++ js/home.js
@@ -0,0 +1,23 @@
+
+
+// $(window).on('click', function() {
+//   // $('#words .h').fadeOut('slow', function() {
+//   //   $('#images .h').fadeIn('slow');
+//   // });
+//   // $('#words .d').fadeOut('slow', function() {
+//   //   $('#images .d').fadeIn('slow');
+//   // });
+//   // $('#words .a').fadeOut('slow', function() {
+//   //   $('#images .a').fadeIn('slow');
+//   // });
+//   // $('#words .y').fadeOut('slow', function() {
+//   //   $('#images .y').fadeIn('slow');
+//   // });
+//   // $('#words .n').fadeOut('slow', function() {
+//   //   $('#images .n').fadeIn('slow');
+//   // });
+
+
+// // });
+// $('#words div').show(0).delay(1000).hide(0);
+// $('#images div').show(1000).delay(2000);
