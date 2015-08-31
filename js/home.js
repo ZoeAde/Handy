b7ec42b138915452b2d5bb739ff526d42a66c7c1
ZoeAde <zoe.a.adelman@gmail.com>
2015-08-31 09:32:18 -0600

    modifying letter and emoji fade time

--- js/home.js
+++ js/home.js
@@ -8,41 +8,7 @@ $(document).on('ready', function() {
   var d = $(".d");
   var y = $(".y");
 
-var hImg = '<img src="emojis/hand2.png">';
-var hLetter = "H";
-
-
-//title words slide down window
-
 // $title.hide();
-// $emojis.hide();
-// $emojis.delay(1000).fadeIn();
-// $title.slideDown(2500);
-// $emojis.slideDown(1500);
-
-// var change = function() {
-
-
-// };
-
-// function fadeLetter(letter, emoji, speed) {
-//   setInterval(function() {letter.fadeOut()}, 3000);
-//   setInterval(function() {emoji.fadeIn()}, 2800);
-
-// var changeDiv = function() {
-//   if ($('#hL').html() === "H") {
-//     $('#hL').html(hImg);
-//   }
-//   else {
-//     $('#hL').html("H");
-//   }
-// };
-
-
-
-// fadeLetter(hLetter, hEmoji);
-
-// setInterval(changeDiv(), 3000);
-
-
+$emojis.hide();
+// $emojis.delay(3000).fadeIn();
 });
